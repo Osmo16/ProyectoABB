e594546aba480ed5da59959a1304cd1dd8923186
Osman <osmandh16@gmail.com>
2025-09-16 21:36:43 -0500

    Clase node

--- EDD2.py
+++ EDD2.py
@@ -1,1 +1,10 @@
 print("hola")
+
+class Node:
+    def __init__(self, poder, name, x=0, y=0):
+        self.power = poder
+        self.name = name
+        self.x = x
+        self.y = y
+        self.left = None
+        self.right = None
